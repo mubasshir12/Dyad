da062038dc7205d4ec00b59573522549179f468c
Will Chen <willchen90@gmail.com>
2025-05-22 22:45:29 -0700

    more logging & install playwright deps

--- e2e-tests/main.spec.ts
+++ e2e-tests/main.spec.ts
@@ -29,10 +29,25 @@ test.beforeAll(async () => {
       //   pw:browser [pid=2743][err] [2743:0523/050346.234089:FATAL:setuid_sandbox_host.cc(163)] The SUID sandbox helper binary was found, but is not configured correctly. Rather than run without sandboxing I'm aborting now. You need to make sure that /home/runner/work/dyad/dyad/out/dyad-linux-x64/chrome-sandbox is owned by root and has mode 4755. +71ms
       "--no-sandbox",
       "--disable-gpu",
+      "--enable-logging",
       "--user-data-dir=/tmp/dyad-e2e-tests",
     ],
     executablePath: appInfo.executable,
   });
+
+  console.log("electronApp launched!");
+
+  // Listen to main process output immediately
+  electronApp.process().stdout?.on("data", (data) => {
+    console.log(`MAIN_PROCESS_STDOUT: ${data.toString()}`);
+  });
+  electronApp.process().stderr?.on("data", (data) => {
+    console.error(`MAIN_PROCESS_STDERR: ${data.toString()}`);
+  });
+  electronApp.on("close", () => {
+    console.log(`Electron app closed listener:`);
+  });
+
   electronApp.on("window", async (page) => {
     const filename = page.url()?.split("/").pop();
     console.log(`Window opened: ${filename}`);
