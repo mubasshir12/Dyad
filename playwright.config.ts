8fb1b8b77186957ab04748acd38a96c7d4afafdb
Will Chen <willchen90@gmail.com>
2025-05-22 22:17:10 -0700

    make debugging failures in CI easier

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,6 +3,19 @@ import { PlaywrightTestConfig } from "@playwright/test";
 const config: PlaywrightTestConfig = {
   testDir: "./e2e-tests",
   maxFailures: 1,
+
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: "html",
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    /* See https://playwright.dev/docs/trace-viewer */
+    trace: "retain-on-failure",
+
+    // Capture screenshot after each test failure.
+    screenshot: "on",
+
+    video: "retain-on-failure",
+  },
 };
 
 export default config;
