b0cd05e21a83ab348828496564a647c4f8058783
Will Chen <willchen90@gmail.com>
2025-05-30 12:00:34 -0700

    split into 1

--- e2e-tests/dump.spec.ts
+++ e2e-tests/dump.spec.ts
@@ -1,7 +1,7 @@
 import { test } from "./helpers/test_helper";
 
 // This is useful to make sure the messages are being sent correctly.
-test("dump messages", async ({ electronApp: _electronApp, po }) => {
+test("dump messages", async ({ po }) => {
   await po.setUp();
   await po.sendPrompt("[dump]");
   await po.snapshotServerDump();
--- e2e-tests/main.spec.ts
+++ e2e-tests/main.spec.ts
@@ -1,12 +1,4 @@
-import { expect } from "@playwright/test";
 import { test } from "./helpers/test_helper";
-
-test("renders the first page", async ({ electronApp }) => {
-  const page = await electronApp.firstWindow();
-  await page.waitForSelector("h1");
-  const text = await page.$eval("h1", (el) => el.textContent);
-  expect(text).toBe("Build your dream app");
-});
 
 test("simple message to custom test model", async ({ po }) => {
   await po.setUp();
